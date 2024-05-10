e30215dc4e541e979fcad6e673e5c9661994cea9
Mateusz Pusty <mateusz.pusty@cdv.pl>
2024-05-10 13:14:16 +0200

    Text UI

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 
@@ -7,8 +8,10 @@ public class BirdController : MonoBehaviour
 {
     public float JumpForce;
     public float MaxVelocityY;
+    public int Points;
 
     public Rigidbody2D Rigidbody2D;
+    public TextMeshProUGUI PointsTextField;
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +32,7 @@ public class BirdController : MonoBehaviour
         {
             Rigidbody2D.velocity = new Vector2(0, MaxVelocityY);
         }
+
+        PointsTextField.text = Points.ToString();
     }
 }
